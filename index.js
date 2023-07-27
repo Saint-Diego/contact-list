3bcaab3382ba890be4e2360fc9f6511558cbc2e0
Diego López Beltrán <ing.diegolopezb@gmail.com>
2023-07-27 11:26:27 -0500

    updated file index for branch project-2

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-// 1. Crea una lista de contactos con datos predefinidos, cada contacto debe contener el nombre y apellido como una sola cadena de caracteres
-const listaContactos = ["Diego Lopez"];
+// 1. Crea una lista de contactos con datos predefinidos, cada contacto debe contener la siguiente información como objetos: { id, nombres, apellidos, teléfono, ubicaciones, ciudad, dirección }.
+const ID = 0;
+const listaContactos = [
+  {
+    id: ID,
+    nombres: "Diego",
+    apellidos: "Lopez",
+    telefono: 3122878866,
+    ubicacion: {
+      ciudad: "Toluviejo",
+      direccion: "Cll. 4 # 3-55",
+    },
+  },
+];
 
 // 2. Crea una función para añadir un nuevo contacto a una lista
-const agregarContacto = (nombres) => {
-  listaContactos.push(nombres);
+const agregarContacto = (nombres, apellidos, telefono = 0, ubicacion) => {
+  const { ciudad = "NA", direccion = "NA" } = ubicacion;
+  const contacto = {
+    id: ++ID,
+    nombres,
+    apellidos,
+    telefono,
+    ubicacion: {
+      ciudad,
+      direccion,
+    },
+  };
+  listaContactos.push(contacto);
 };
 
 // 3. Crea una función para borrar un contacto existente de la lista
-const eliminarContacto = (contacto) => {
-  let index = listaContactos.indexOf(contacto);
+const eliminarContacto = (id) => {
+  let index = listaContactos.findIndex((c) => c.id === id);
   if (index >= 0) listaContactos.splice(index, 1);
   else
     console.log(
@@ -19,16 +42,18 @@ const eliminarContacto = (contacto) => {
 // 4. Crea una función para imprimir en consola los contactos presentes en la lista
 const listarContactos = () => {
   for (let i = 0; i < listaContactos.length; i++) {
-    console.log(`${i + 1}. ${listaContactos[i]}`);
+    console.log(
+      `${i + 1}. ${listaContactos[i].nombres} ${listaContactos[i].apellidos}`
+    );
   }
   console.log("\n");
 };
 
 listarContactos();
-agregarContacto("Juan Perez");
+agregarContacto("Juan", "Perez", 32566993, {ciudad: "San Luis", direccion: "Dg. 5 cra. 8-12"});
 listarContactos();
-agregarContacto("Daniel Jimenez");
+agregarContacto("Daniel", "Jimenez");
 listarContactos();
-eliminarContacto("Diego Lopez");
-// eliminarContacto("Diego Lopes");
+eliminarContacto(0);
+// eliminarContacto(-1);
 listarContactos();
