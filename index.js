b9feb8982a39c5d2628e06b93e544071ab4e5ce2
Diego López Beltrán <ing.diegolopezb@gmail.com>
2023-07-27 10:11:32 -0500

    changed file index

--- index.js
+++ index.js
@@ -15,22 +15,22 @@ const agregarContacto = (nombres) => {
 
 // 3. Crea una función para borrar un contacto existente de la lista
 const eliminarContacto = (contacto) => {
-  let index = listaContactos.findIndex(c => c.nombres === contacto);
+  let index = listaContactos.findIndex((c) => c.nombres === contacto);
   if (index >= 0) listaContactos.splice(index, 1);
 };
 
 // 4. Crea una función para imprimir en consola los contactos presentes en la lista
 const listarContactos = () => {
   for (let i = 1; i <= listaContactos.length; i++) {
-    console.log(`${i}. ${listaContactos[i-1].nombres}`);
+    console.log(`${i}. ${listaContactos[i - 1].nombres}`);
   }
   console.log("\n");
-}
+};
 
 listarContactos();
 agregarContacto("Juan Perez");
 listarContactos();
-agregarContacto("Daniel Jimenez")
+agregarContacto("Daniel Jimenez");
 listarContactos();
-eliminarContacto("Diego Lopez")
+eliminarContacto("Diego Lopez");
 listarContactos();
