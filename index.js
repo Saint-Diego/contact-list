7e2de2d5015d918421ba886888586f181ac7a013
Diego López Beltrán <ing.diegolopezb@gmail.com>
2023-07-27 10:27:20 -0500

    changed file index

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 // 1. Crea una lista de contactos con datos predefinidos, cada contacto debe contener el nombre y apellido como una sola cadena de caracteres
-const listaContactos = [
-  {
-    nombres: "Diego Lopez",
-  },
-];
+const listaContactos = ["Diego Lopez"];
 
 // 2. Crea una función para añadir un nuevo contacto a una lista
 const agregarContacto = (nombres) => {
-  const contacto = {
-    nombres,
-  };
-  listaContactos.push(contacto);
+  listaContactos.push(nombres);
 };
 
 // 3. Crea una función para borrar un contacto existente de la lista
 const eliminarContacto = (contacto) => {
-  let index = listaContactos.findIndex((c) => c.nombres === contacto);
+  let index = listaContactos.indexOf(contacto);
   if (index >= 0) listaContactos.splice(index, 1);
+  else
+    console.log(
+      `El contacto a eliminar "${contacto}" no se encuentra registrado \n`
+    );
 };
 
 // 4. Crea una función para imprimir en consola los contactos presentes en la lista
 const listarContactos = () => {
-  for (let i = 1; i <= listaContactos.length; i++) {
-    console.log(`${i}. ${listaContactos[i - 1].nombres}`);
+  for (let i = 0; i < listaContactos.length; i++) {
+    console.log(`${i + 1}. ${listaContactos[i]}`);
   }
   console.log("\n");
 };
@@ -33,4 +30,5 @@ listarContactos();
 agregarContacto("Daniel Jimenez");
 listarContactos();
 eliminarContacto("Diego Lopez");
+// eliminarContacto("Diego Lopes");
 listarContactos();
