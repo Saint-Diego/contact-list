52f45609b5e6c3d2a067a8961a21b4830e33cbbc
Diego López Beltrán <ing.diegolopezb@gmail.com>
2023-07-27 16:32:27 -0500

    second update file index for branch project-2

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // 1. Crea una lista de contactos con datos predefinidos, cada contacto debe contener la siguiente información como objetos: { id, nombres, apellidos, teléfono, ubicaciones, ciudad, dirección }.
-const ID = 0;
+let ID = 0;
 const listaContactos = [
   {
     id: ID,
@@ -14,8 +14,13 @@ const listaContactos = [
 ];
 
 // 2. Crea una función para añadir un nuevo contacto a una lista
-const agregarContacto = (nombres, apellidos, telefono = 0, ubicacion) => {
-  const { ciudad = "NA", direccion = "NA" } = ubicacion;
+const agregarContacto = (
+  nombres,
+  apellidos,
+  telefono = 0,
+  ciudad = "NA",
+  direccion = "NA"
+) => {
   const contacto = {
     id: ++ID,
     nombres,
@@ -31,29 +36,38 @@ const agregarContacto = (nombres, apellidos, telefono = 0, ubicacion) => {
 
 // 3. Crea una función para borrar un contacto existente de la lista
 const eliminarContacto = (id) => {
-  let index = listaContactos.findIndex((c) => c.id === id);
-  if (index >= 0) listaContactos.splice(index, 1);
-  else
-    console.log(
-      `El contacto a eliminar "${contacto}" no se encuentra registrado \n`
-    );
+  if (!isNaN(id)) {
+    let index = listaContactos.findIndex((c) => c.id === id);
+    if (index >= 0) listaContactos.splice(index, 1);
+    else console.log(`ID ${id} no válido`);
+  } else console.log(`ID "${id}" del contacto a eliminar no es existe`);
 };
 
 // 4. Crea una función para imprimir en consola los contactos presentes en la lista
 const listarContactos = () => {
   for (let i = 0; i < listaContactos.length; i++) {
+    const {
+      id,
+      nombres,
+      apellidos,
+      telefono,
+      ubicacion: { ciudad, direccion },
+    } = listaContactos[i];
     console.log(
-      `${i + 1}. ${listaContactos[i].nombres} ${listaContactos[i].apellidos}`
+      `${
+        i + 1
+      }. ID: ${id} - Nombre completo: ${nombres} ${apellidos} - Teléfono: ${telefono} - Dirección: ${direccion}, ${ciudad}`
     );
   }
   console.log("\n");
 };
 
 listarContactos();
-agregarContacto("Juan", "Perez", 32566993, {ciudad: "San Luis", direccion: "Dg. 5 cra. 8-12"});
+agregarContacto("Juan", "Perez", 32566993, "San Luis", "Dg. 5 cra. 8-12");
 listarContactos();
 agregarContacto("Daniel", "Jimenez");
 listarContactos();
 eliminarContacto(0);
 // eliminarContacto(-1);
+// eliminarContacto("tg");
 listarContactos();
